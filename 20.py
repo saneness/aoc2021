0a1af73938ea4eef8941e69f24130c28c4551669
saneness <saneness@protonmail.com>
2021-12-20 17:52:29 +0300

    update

--- 20.py
+++ 20.py
@@ -1,13 +1,17 @@
 data = open("data/20.data").read().replace("#", "1").replace(".", "0").split("\n\n")
-data[1] = data[1].split("\n")
+data[0] = [int(item) for item in data[0]]
+data[1] = [[int(subitem) for subitem in item] for item in data[1].split("\n")]
 
 def solve():
     def extend(image, n, symbol):
+        extended = []
+        for row in image:
+            extended.append([symbol] * n + row + [symbol] * n)
+        x_size = len(extended[0])
         for _ in range(n):
-            image = [row.join([symbol]*2) for row in image]
-            x_size = len(image[0])
-            image = [symbol*x_size] + image + [symbol*x_size]
-        return image
+            extended.insert(0, [symbol] * x_size)
+            extended.insert(len(extended), [symbol] * x_size)
+        return extended
 
     def crop(image):
         while image[0] == image[1]:
@@ -21,19 +25,19 @@ def solve():
         return image
 
     def lit(image):
-        return sum([sum([1 for symbol in row if symbol == "1"]) for row in image])
+        return sum([sum(row) for row in image])
 
     def enhance(image, pixel_map, steps):
-        image = extend(image, 3, "0")
+        image = extend(image, 3, 0)
         y_size = len(image)
         x_size = len(image[0])
-        for _ in range(steps):
-            new_image = [["0" for _ in range(x_size)] for _ in range(y_size)]
+        for step in range(steps):
+            new_image = [[0 for _ in range(x_size)] for _ in range(y_size)]
             for row in range(1, y_size - 1):
                 for col in range(1, x_size - 1):
-                    pixel = int("".join([image[i][j] for i in range(row - 1, row + 2) for j in range(col - 1, col + 2)]), 2)
+                    pixel = int("".join([str(image[i][j]) for i in range(row - 1, row + 2) for j in range(col - 1, col + 2)]), 2)
                     new_image[row][col] = pixel_map[pixel]
-            new_image = ["".join(row[1:-1]) for row in new_image[1:-1]]
+            new_image = [row[1:-1] for row in new_image[1:-1]]
             image = crop(new_image)
             image = extend(image, 2, image[0][0])
             y_size = len(image)
